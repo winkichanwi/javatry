7e89265a8358b12df128146a37bef2230840c2bb
Wing In CHAN <wingin.chan@bizreach.co.jp>
2020-03-25 18:43:51 +0900

    Step07 test_exception_checkedException_basic

--- src/test/java/org/docksidestage/javatry/basic/Step07ExceptionTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step07ExceptionTest.java
@@ -15,6 +15,9 @@
  */
 package org.docksidestage.javatry.basic;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.docksidestage.bizfw.basic.supercar.SupercarClient;
 import org.docksidestage.javatry.basic.st7.St7ConstructorChallengeException;
 import org.docksidestage.unit.PlainTestCase;
@@ -90,6 +93,13 @@ public class Step07ExceptionTest extends PlainTestCase {
      * (new java.io.File(".") の canonical path を取得してログに表示、I/Oエラーはメッセージとスタックトレースを代わりに)
      */
     public void test_exception_checkedException_basic() {
+        File file = new java.io.File(".");
+        try {
+            log(file.getCanonicalPath());
+        } catch (IOException e) {
+            log(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     // ===================================================================================
