0d44693c7c262bf9fed94a22a4c652fd69bffdd9
Wing In CHAN <wingin.chan@bizreach.co.jp>
2020-05-13 13:28:36 +0900

    Step13 first q

--- src/test/java/org/docksidestage/javatry/colorbox/Step13NumberTest.java
+++ src/test/java/org/docksidestage/javatry/colorbox/Step13NumberTest.java
@@ -15,6 +15,10 @@
  */
 package org.docksidestage.javatry.colorbox;
 
+import java.util.List;
+
+import org.docksidestage.bizfw.colorbox.ColorBox;
+import org.docksidestage.bizfw.colorbox.yours.YourPrivateRoom;
 import org.docksidestage.unit.PlainTestCase;
 
 /**
@@ -33,6 +37,15 @@ public class Step13NumberTest extends PlainTestCase {
      * (カラーボックの中に入っているInteger型で、0から54までの値は何個ある？)
      */
     public void test_countZeroToFiftyFour_IntegerOnly() {
+        List<ColorBox> colorBoxList = new YourPrivateRoom().getColorBoxList();
+        long count = colorBoxList.stream()
+                .flatMap(colorBox -> colorBox.getSpaceList().stream())
+                .map(boxSpace -> boxSpace.getContent())
+                .filter(content -> content instanceof Integer)
+                .map(content -> (Integer) content)
+                .filter(integer -> integer >= 0 && integer <= 54)
+                .count();
+        log("No of integer-type values in color-boxes between 0 and 54: " + count);
     }
 
     /**
